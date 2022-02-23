0bce793a0f25d102624587a6e14458af6863ed09
Antonio Franco <antoniofranco.dev@gmail.com>
2022-02-23 00:57:43 -0300

    heroe service

---
+++ src/app/home/service/heroe.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroeService {
+  urlBase = environment.urlApi + environment.tokenApi + '/'
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getHeroByName(name:string){
+    const url = `${this.urlBase}search/${name}`
+    return this.http.get(url)
+  }
+  getConnectionsHeroeById(id:number){
+    const url = `${this.urlBase}${id}/connections`
+    return this.http.get(url)
+  }
+  getWorkHeroeById(id:number){
+    const url = `${this.urlBase}${id}/work`
+    return this.http.get(url)
+  }
+
+  getAppearanceHeroeById(id:number){
+    const url= `${this.urlBase}${id}/appearance`
+    this.http.get(url)
+  }
+}
