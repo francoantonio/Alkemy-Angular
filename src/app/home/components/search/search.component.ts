bc0a4a446fc705b08a71c720b5140bac60754aee
Antonio Franco <antoniofranco.dev@gmail.com>
2022-02-23 00:45:18 -0300

    componente buscador

--- src/app/home/components/search/search.component.ts
+++ src/app/home/components/search/search.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
-
+export class SearchComponent implements OnInit, OnDestroy {
+  subscription !: Subscription
+  element = new FormControl('')
+  @Input() placeholder:string =' '
+  @Output()nextValue:EventEmitter<string> =new  EventEmitter<string>()
   constructor() { }
-
   ngOnInit(): void {
+    this.subscription = this.element.valueChanges
+    .pipe(
+      debounceTime(400))
+      .subscribe((e)=>{
+        this.nextValue.next(e)
+    })
   }
-
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
